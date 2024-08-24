f44f42450c557bb930d1056a18a2bb52f4275123
Tanaya97-maker <ritvis97@gmail.com>
2024-08-24 18:40:43 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,10 +12,12 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet"/>
     </Head>
-    <Component {...pageProps} />
-    <SideNavbar/>
+    <div className=" flex ">  
+        <Component {...pageProps} className="flex-1 "/>
+    </div>
     <Sidebar/>
-    <Footer/>
+    <Footer />
+    <SideNavbar/>
     </>
   )
 }
